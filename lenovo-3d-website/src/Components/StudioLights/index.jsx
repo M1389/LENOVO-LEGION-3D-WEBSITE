8ae5353dcf6e3b46f075897bc864fa0d9f3df56b
Mahdi Hashemi <95425009+M1389@users.noreply.github.com>
2025-12-09 23:23:23 +0400

    add 3d model and components

---
+++ lenovo-3d-website/src/Components/StudioLights/index.jsx
@@ -0,0 +1,36 @@
+import { Environment, Lightformer, SpotLight } from '@react-three/drei'
+import React from 'react'
+
+export default function StudioLights() {
+  return (
+    <group>
+       <Environment>
+        <group>
+            <Lightformer 
+            form='rect'
+            intensity={10}
+            position={[10,5,-5]}
+            scale={10}
+            />
+        </group>
+        <group>
+            <Lightformer 
+            form='rect'
+            intensity={10}
+            position={[-10,5,-5]}
+            scale={10}
+            />
+        </group>
+        <group>
+            <Lightformer 
+            form='rect'
+            intensity={10}
+            position={[10,-5,-5]}
+            scale={10}
+            />
+        </group>
+        </Environment> 
+        
+    </group>
+  )
+}
--- lenovo-3d-website/src/Pages/Home/ModelViewer/index.jsx
+++ lenovo-3d-website/src/Pages/Home/ModelViewer/index.jsx
@@ -1,16 +1,44 @@
-import { Box, OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { Lenovo } from '../../../Components/Models/Lenovo/Lenovo'
+import { Box, OrbitControls, PresentationControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { Legion } from "../../../Components/Models/Lenovo/LEGION";
+import StudioLights from "../../../Components/StudioLights";
+
 
 export default function ModelViewer() {
+
+
+  
   return (
-    <section className='bg-white py-12 min-h-[1000px]'>
-        <Canvas className='h-full' camera={{position: [0,2,5] ,fov: 100 , near: 0.1 , far:50 }}>
-            <Lenovo scale={0.2} position={[0,-1,0]}/>
-            <OrbitControls enableZoom={false}/>
-            
-        </Canvas>
+    <section className="bg-black py-12 ">
+      <div className="py-11 flex flex-col items-center">
+        <h2 className="text-4xl Orbitron text-white text-center tracking-wider">Take a colser look</h2>
+        <img className="w-[55%] mx-auto" src="/ModelPreview/RGB-closerlook-2.png" alt="RGB-light" />
+      </div>
+      <Canvas
+        id="canvas"
+        camera={{
+          position: [0, 35, 30], // Camera above and behind
+          fov: 70,
+          near: 0.1,
+          far: 1000,
+        }}
+      >
+        <StudioLights />
+        <PresentationControls 
+          snap={true} 
+          speed={1} 
+          zoom={1} 
+          polar={[-Math.PI , Math.PI]}
+          global={true}
+          config={{ mass: 2, tension: 400 }} 
+          >
+          <group position={[-25.15, -9.99, 10.151]} scale={120}>
+            <Legion />
+          </group>
+        </PresentationControls>
+        
+      </Canvas>
     </section>
-  )
+  );
 }
