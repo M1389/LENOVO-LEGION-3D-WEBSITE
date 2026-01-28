88289bbcaf6448479dd55ef185e8f1cff5bba407
Mahdi Hashemi <95425009+M1389@users.noreply.github.com>
2026-01-28 13:06:55 +0400

    offer section created

---
+++ lenovo-3d-website/src/Pages/Home/OfferSection/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function OfferSection() {
+  return (
+    <section>
+        <h2>IF YOU ARE</h2>
+        <div>
+            
+        </div>
+    </section>
+  )
+}
